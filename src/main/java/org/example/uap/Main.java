934c8380d97afd7420dfeedcf45488c7c5cb043c
NusratMome <nusratjahanmome345@gmail.com>
2024-05-03 15:33:11 +0600

    dekh

--- src/main/java/org/example/uap/Main.java
+++ src/main/java/org/example/uap/Main.java
@@ -3,5 +3,6 @@ package org.example.uap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Junaiderbacchaaaa");
+        System.out.println("kidibo");
     }
 }
