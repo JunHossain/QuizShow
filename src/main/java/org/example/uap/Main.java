035ac022359ef74935dd6a579fcd82c67bfdf709
NusratMome <nusratjahanmome345@gmail.com>
2024-05-03 16:00:48 +0600

    change

--- src/main/java/org/example/uap/Main.java
+++ src/main/java/org/example/uap/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Junaiderbacchaaaa");
         System.out.println("kiidibo");
-        System.out.println("Hellow");
+        System.out.println("Hellow world");
+
     }
 }
