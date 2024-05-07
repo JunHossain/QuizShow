dafd1eea665e9444c186ad91bf9d395a0659c148
Jun Hossain <junaidhossain45@gmail.com>
2024-05-07 13:38:32 +0600

    dead

--- src/main/java/org/example/uap/Main.java
+++ src/main/java/org/example/uap/Main.java
@@ -3,5 +3,6 @@ package org.example.uap;
 public class Main {
     public static void main(String[] args) {
         new Menu().setVisible(true);
+        System.out.println("Hello World!");
     }
 }
